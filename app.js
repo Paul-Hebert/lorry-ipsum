fd1204c6d6ad68bf27d80784a59d82bd9e004829
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2018-09-01 10:41:34 -0700

    Began populating words. Removed cookie parser I won't be using
    These aren’t the real words I’m going to use.

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
