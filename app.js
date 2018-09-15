3b074f7c3058cb97c1677fb24960939925831869
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2018-09-15 11:05:11 -0700

    Adding port logic for heroic

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ var generateRouter = require('./routes/generate');
 
 var app = express();
 
+// Logic to set port
+app.set('port', (process.env.PORT || 5000));
+
+//For avoiding Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
